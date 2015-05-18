a8ab566de9af4710afad9d69f48d496f1cd05219
MIchael <MIchael@MWEV>
2015-05-18 09:14:06 -0400

    Test code mode release

---
+++ resources/views/store/productInfo.blade.php
@@ -0,0 +1,60 @@
+@extends('store.layout.masterCatalog')
+
+<?php
+?>
+@section('content')
+<div>
+    <div class="col-md-6">
+        <div class="img-responsive">
+            <img class='image-resize-responsive' src='{{$images[0]->getUrl()}}'/> 
+        </div>
+    </div>
+    <div class="col-md-6">
+        <h2>{{$catalog->name}}</h2>
+        <div class="col-md-12">
+            {{$catalog->description->description}}
+        </div>
+        <div class="col-md-12">
+            <h2>${{$price->price}}</h2>
+        </div>
+        <div>
+            <form class="form-inline">
+                <div class="col-md-4 col-xs-6>
+                    <label for="quantity">Qty:</label>
+                    <input type="text"  style="width: 15%"  id="quantity" value="1">
+                </div>
+                <div class="col-md-8 col-xs-6">
+                    
+                        <button class="btn btn-success btn-large">
+                            <i class="icon-white icon-shopping-cart"></i> 
+                            Add to Cart</button>
+                    
+                    
+                        <button class="controls form-inline btn btn-success btn-large">
+                            <i class="icon-white icon-shopping-cart"></i> 
+                            Add to Cart</button>
+                </div>   
+                      
+            </form>
+        </div>
+        <div class="col-md-12">
+            <form class="form-horizontal">
+
+                <div class="control-group">
+                    <div class="controls form-inline">
+                        <label for="inputKey">Key</label>
+                        <input type="text" class="input-small"  placeholder="Key" id="inputKey">
+                        <button class="controls form-inline btn btn-success btn-large">
+                            <i class="icon-white icon-shopping-cart"></i> 
+                            Add to Cart</button>
+                        <button class="controls form-inline btn btn-success btn-large">
+                            <i class="icon-white icon-shopping-cart"></i> 
+                            Add to Cart</button>
+                    </div>
+                </div>
+            </form>
+        </div>
+
+    </div>
+</div>
+@endsection
