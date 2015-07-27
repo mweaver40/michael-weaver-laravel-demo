58738b4bd241ab30d942ff4df45cd04ec9179d26
MIchael <MIchael@MWEV>
2015-07-27 12:08:51 -0400

    Test

--- config/database.php
+++ config/database.php
@@ -54,9 +54,9 @@ return [
 
 		'mysql' => [
 			'driver'    => 'mysql',
-			'host'      => env('DB_HOST', 'localhost'),
-			'database'  => env('DB_DATABASE', 'scuba'),
-			'username'  => env('DB_USERNAME', 'root'),
+			'host'      => env('DB_HOST', ''),
+			'database'  => env('DB_DATABASE', ''),
+			'username'  => env('DB_USERNAME', ''),
 			'password'  => env('DB_PASSWORD', ''),
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
